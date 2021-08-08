6f62e50c0a2b6817b20b642d540d079046ad6372
Tyler Hou <tylerhou@google.com>
2021-08-07 23:50:20 -0700

    Add configuration.nix and setup & clean scripts.

---
+++ nix-dotfiles/clean.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+
+rm $HOME/.config/nixpkgs
+sudo rm /etc/nixos/configuration.nix
---
+++ nix-dotfiles/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+set -e
+
+ln -s $PWD/home-manager $HOME/.config/nixpkgs
+sudo ln -s $PWD/configuration.nix /etc/nixos/configuration.nix
